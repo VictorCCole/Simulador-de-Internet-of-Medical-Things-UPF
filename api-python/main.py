76ac2969edf6babdff71b3e5667a4f7bac0edd1d
Pedro Marcelo Roso Manica <43484903+Weyzert@users.noreply.github.com>
2024-11-22 18:40:00 -0300

    Update main.py
    Agora o CORS não vai mais ser um problema.

--- api-python/main.py
+++ api-python/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routes.dados_coletados import router as dados_coletados_router
 from db.database import criar_tabelas
+from starlette.middleware.cors import CORSMiddleware
 
 criar_tabelas()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(dados_coletados_router, prefix="/dadosColetados")
