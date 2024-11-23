f7d5f2d63112d0424fe31cceebf8f06c4c8e94be
guirodolfo <54751935+guirodolfo@users.noreply.github.com>
2024-11-23 17:54:55 -0300

    Update usuario_schema.py

--- api-python/schemas/usuario_schema.py
+++ api-python/schemas/usuario_schema.py
@@ -1,17 +1,5 @@
-from sqlalchemy import Column, Integer, String, Date, Float
-from db.database import Base
 from pydantic import BaseModel
 from datetime import date
-
-class UsuarioDB(Base):
-    __tablename__ = "Usuario"
-
-    codigo = Column(Integer, primary_key=True, index=True)
-    Nome = Column(String(50), nullable=False)
-    Nascimento = Column(Date, nullable=False)
-    Sexo = Column(String(1), nullable=False)
-    Latitude = Column(Float, nullable=True)
-    Longitude = Column(Float, nullable=True)
 
 class Usuario(BaseModel):
     codigo: int
