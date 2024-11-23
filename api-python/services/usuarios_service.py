5a6a3ac912d4cb205f8d4bfaed69eb848ec2f19a
guirodolfo <54751935+guirodolfo@users.noreply.github.com>
2024-11-23 17:55:47 -0300

    Update usuarios_service.py

--- api-python/services/usuarios_service.py
+++ api-python/services/usuarios_service.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from models.usuario_model import Usuario
+from models.usuario_model import UsuarioDB
 from schemas.usuario_schema import Usuario
 from repositories.usuarios_repository.py import (
     obter_usuarios,
@@ -9,17 +9,17 @@ from repositories.usuarios_repository.py import (
     deletar_usuario
 )
 
-def create_usuario_service(db: Session):
-    return criar_usuario(db)
+def create_usuario_service(db: Session, dados: Usuario):
+    return criar_usuario(db, dados)
 
-def get_usuarios_service(db: Session, skip: int, limit: int):
-    return obter_usuarios(db, skip, limit)
+def get_usuarios_service(db: Session):
+    return obter_usuarios(db)
 
 def get_usuario_service(db: Session, usuario_id: int):
     return obter_usuario_por_seq(db, usuario_id)
 
-def update_usuario_service(db: Session, usuario_id: int):
-    return atualizar_usuario(db, usuario_id)
+def update_usuario_service(db: Session, usuario_id: int, dados: Usuario):
+    return atualizar_usuario(db, usuario_id, dados)
 
 def delete_usuario_service(db: Session, usuario_id: int):
     return deletar_usuario(db, usuario_id)
