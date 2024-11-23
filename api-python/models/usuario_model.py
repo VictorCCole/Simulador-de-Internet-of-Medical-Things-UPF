422034791d45627ea1ab979c15a305f6533af410
guirodolfo <54751935+guirodolfo@users.noreply.github.com>
2024-11-23 17:54:30 -0300

    Update usuario_model.py

--- api-python/models/usuario_model.py
+++ api-python/models/usuario_model.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Integer, String, Date, Float
 from db.database import Base
-from pydantic import BaseModel
-from datetime import date
 
 class UsuarioDB(Base):
     __tablename__ = "Usuario"
@@ -12,14 +10,3 @@ class UsuarioDB(Base):
     Sexo = Column(String(1), nullable=False)  # 'M' ou 'F'
     Latitude = Column(Float, nullable=True)
     Longitude = Column(Float, nullable=True)
-
-class Usuario(BaseModel):
-    codigo: int
-    Nome: str
-    Nascimento: date
-    Sexo: str
-    Latitude: float = None
-    Longitude: float = None
-
-    class Config:
-        orm_mode = True
